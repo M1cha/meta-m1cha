d66b8e4fcc7355cc0aab521a22f7a4d0a2578e11
Michael Zimmermann <sigmaepsilon92@gmail.com>
2020-03-24 19:12:09 +0100

    add west command "yocto"

---
+++ scripts/west_commands/yocto.py
@@ -0,0 +1,32 @@
+from west.commands import WestCommand
+from west import log
+from west import util
+from pathlib import Path
+import os
+import subprocess
+
+class Yocto(WestCommand):
+    def __init__(self):
+        super().__init__(
+            "yocto",
+            "",
+            None)
+
+        self.top_dir = util.west_topdir()
+        self.project_dir = Path(os.path.dirname(os.path.realpath(__file__))).parent.parent
+
+    def do_add_parser(self, parser_adder):
+        return parser_adder.add_parser(self.name,
+                                       add_help=True,
+                                       description=self.description)
+
+    def do_run(self, args, unknown_args):
+        args = [
+            os.path.join(self.project_dir, 'scripts/yoctoshell.sh'),
+        ]
+
+        p = subprocess.Popen(args, cwd=self.top_dir)
+        p.communicate()
+
+        if p.returncode:
+            raise subprocess.CalledProcessError(p.returncode, args)
---
+++ scripts/yoctoshell.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export TEMPLATECONF="${PWD}/meta-m1cha/conf"
+source ./poky/oe-init-build-env
+exec bash
